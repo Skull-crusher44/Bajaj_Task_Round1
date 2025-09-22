fb326a6df9a6f1c1b297c40a44f21bb7d4a9fe8b
Skull-crusher44 <pankajahuja2212@gmail.com>
2025-09-22 15:45:07 +0530

    Little change please accept it

--- src/main/java/com/Bajaj_Exam/Pankaj/dto/GenerateWebhookResponse.java
+++ src/main/java/com/Bajaj_Exam/Pankaj/dto/GenerateWebhookResponse.java
@@ -1,11 +1,14 @@
 package com.Bajaj_Exam.Pankaj.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class GenerateWebhookResponse {
+    @JsonProperty("webhook")
     private String webhookURL;
     private String accessToken;
 }
